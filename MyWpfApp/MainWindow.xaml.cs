d65f66b470a7b8e51efbdf8c52966cfd3ba726ad
statking <60002940+kimwoonggon@users.noreply.github.com>
2026-03-26 09:51:34 +0900

    Initial WPF project setup

---
+++ MyWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace MyWpfApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void LoginButton_Click(object sender, RoutedEventArgs e)
+    {
+        var userName = UserNameTextBox.Text;
+        var password = PasswordTextBox.Text;
+
+        MessageBox.Show(
+            $"아이디: {userName}\n비밀번호: {password}",
+            "입력한 로그인 정보",
+            MessageBoxButton.OK,
+            MessageBoxImage.Information);
+    }
+}
